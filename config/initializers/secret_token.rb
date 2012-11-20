0c8201f5f1cfedca7bae4d8e6d4ced6b0d233c6a
tom <git@wonderingmachine.com>
2012-11-20 12:08:34 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpringyTest::Application.config.secret_token = 'cdcae858d8d4d145425322fab0efb66224b366be15fc56d657a50499478b4cc8115b6ab2ae0d9e34e66a29062f8b77702536e3ad94ff7b763972bb11bf04402a'
